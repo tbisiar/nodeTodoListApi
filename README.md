beaba9697b3c9af19e2d7614238415439015c76d
Terry Bisiar <t.bisiar@auckland.ac.nz>
2017-12-10 15:54:27 +1300

    Working commit, cleaning up code and starting to add swagger spec

--- README.md
+++ README.md
@@ -1,15 +1,17 @@
 # nodeTodoListApi
-Test repo for node RESTful API
+This is a demo repo for node RESTful API built based on tutorial at [codementor](https://www.codementor.io/olatundegaruba/nodejs-restful-apis-in-10-minutes-q0sgsfhbd)
+
+with swagger bottom-up spec generation from [cloudboost](https://blog.cloudboost.io/adding-swagger-to-existing-node-js-project-92a6624b855b)
 
 Start mongodb server:
-mongod
+`mongod`
 
 Run with:
-npm run start
+`npm run start`
 
 Restart with:
-rs
+`rs` (hotswap of code enabled with nodemon)
 
 Test via postman at:
-http://localhost:3000/tasks
+`http://localhost:3000/tasks`
 
--- server.js
+++ server.js
@@ -1,18 +1,26 @@
 var express = require('express'),
-  app = express(),
-  port = process.env.PORT || 3000;
-  mongoose = require('mongoose'),
-  Task = require('./api/models/todoListModel'), //created model loading here
-  bodyParser = require('body-parser');
+    app = express(),
+    port = process.env.PORT || 3000;
+var mongoose = require('mongoose'),
+    Task = require('./api/models/todoListModel'), // created model loading here
+    bodyParser = require('body-parser');
 
 // mongoose instance connection url connection
 mongoose.Promise = global.Promise;
-mongoose.connect('mongodb://localhost/Tododb');
+mongoose.connect('mongodb://localhost/Tododb', {
+    useMongoClient: true,
+});
 
-app.use(bodyParser.urlencoded({ extended: true }));
+// swagger-ui-express setup
+var swaggerUi = require('swagger-ui-express'),
+    swaggerDocument = require('./swagger.json');
+
+app.use(bodyParser.urlencoded({extended: true}));
 app.use(bodyParser.json());
-app.use(function(req, res) {
-  res.status(404).send({url: req.originalUrl + ' not found'})
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
+// app.use('/api/v1', router);
+app.use(function (req, res) {
+    res.status(404).send({url: req.originalUrl + ' not found'})
 });
 
 var routes = require('./api/routes/todoListRoutes'); // importing routes
